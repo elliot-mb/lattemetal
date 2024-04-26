e5458c916ea6d9a500062dbea7783d62f9556ab1
elliot-mb <elliotbuckingham14@gmail.com>
2024-04-26 15:13:25 +0100

    one bit seems ok

--- Processor.java
+++ Processor.java
@@ -10,14 +10,14 @@ public class Processor {
     }
     //@@@SETTINGS@@@
     private static final double CLOCK_SPEED_MHZ = 500;
-    public static final predictor PREDICTOR = predictor.fixedNotTaken;
+    public static final predictor PREDICTOR = predictor.oneBit;
     private static final int BTB_CACHE_SIZE = 32;
     public static final int SUPERSCALAR_WIDTH = 8;
     private static final int ALU_COUNT = 4;
     private static final int LSU_COUNT = 2;
     private static final int BRU_COUNT = 2;
-    private static final int ALU_RS_COUNT = 2;
-    private static final int LSU_RS_COUNT = 1;
+    private static final int ALU_RS_COUNT = 4;
+    private static final int LSU_RS_COUNT = 4;
     private static final int BRU_RS_COUNT = 1;
     private static final int DP_ACC = 4;
     public static final int ROB_ENTRIES = 32;
